52fa3e35361253bf9c5a9b468e92a1dd915ffbd3
Wyatt Barnes <me@wyatt.email>
2025-05-15 21:46:05 -1000

    wip: wrapping policy in handler. Update esbuild and ipfs deployment scripts

--- packages/vincent-tools/vincent-tool-sdk/src/index.ts
+++ packages/vincent-tools/vincent-tool-sdk/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/types';
 export * from './lib/policyCore/vincentPolicy';
 export * from './lib/toolCore/vincentTool';
+export * from './lib/handlers/vincentPolicyHandler';
+export * from './lib/handlers/vincentToolHandler';
