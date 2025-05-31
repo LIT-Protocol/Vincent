d8a3755d0867e375dbe2fe4e321a001003f0f84a
Daryl Collins <daryl@litprotocol.com>
2025-05-31 20:41:08 +0100

    feat(vincent-rest-api): Export Zod schemas of doc defs from the package as `docSchemas`

--- packages/vincent-rest-api/src/index.ts
+++ packages/vincent-rest-api/src/index.ts
@@ -3,3 +3,17 @@ import openApiJson from './generated/openapi.json';
 
 export { vincentApiClient } from './generated/vincentApiClient';
 export { openApiJson, openAPIRegistry };
+
+import { AppDef, AppVersionDef, AppToolDef } from './lib/schemas/app';
+import { ToolDef, ToolVersionDef } from './lib/schemas/tool';
+import { PolicyDef, PolicyVersionDef } from './lib/schemas/policy';
+
+export const docSchemas = {
+  AppDef,
+  AppVersionDef,
+  AppToolDef,
+  ToolDef,
+  ToolVersionDef,
+  PolicyDef,
+  PolicyVersionDef,
+};
