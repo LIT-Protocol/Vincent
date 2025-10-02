3956a62d071e21f8e0cc2d4586a28848e97317e0
FedericoAmura <federicoamura@gmail.com>
2025-10-02 18:05:41 +0200

    feat: remove unused libraries and organize code

--- packages/apps/ability-aave-smart-account/src/lib/helpers/validation.ts
+++ packages/apps/ability-aave-smart-account/src/lib/helpers/validation.ts
@@ -1,4 +1,15 @@
-import { type UserOp } from './userOperation';
+import { ethers } from 'ethers';
+
+import { getAaveAddresses, getATokens } from './aave';
+import { assertValidEntryPointAddress, getSmartAccountNonce } from './entryPoint';
+import {
+  estimateUserOperationGas,
+  getUserOpInitCode,
+  getUserOpVersion,
+  simulateUserOp,
+  UserOp,
+  UserOpv060,
+} from './userOperation';
 import { SimulateUserOperationAssetChangesResponse } from './simulation';
 
 interface ValidateSimulationParams {
@@ -77,4 +88,78 @@ export const validateSimulation = ({
   });
 
   return true;
+};
+
+interface ProccessUserOpParams {
+  entryPointAddress: string;
+  userOp: UserOp;
+  rpcUrl: string;
+}
+
+export const validateUserOp = async (params: ProccessUserOpParams) => {
+  const { entryPointAddress, userOp, rpcUrl } = params;
+  const _userOp = { ...userOp };
+
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+
+  await assertValidEntryPointAddress(entryPointAddress, provider);
+
+  const network = await provider.detectNetwork();
+  const { POOL: aavePoolAddress } = getAaveAddresses(network.chainId);
+  const aaveATokens = getATokens(network.chainId);
+
+  const userOpVersion = getUserOpVersion(_userOp);
+
+  // Complete userOp optional fields
+  if (!_userOp.nonce) {
+    _userOp.nonce = ethers.utils.hexValue(
+      await getSmartAccountNonce({
+        entryPointAddress,
+        provider,
+        accountAddress: _userOp.sender,
+      }),
+    );
+  }
+  if (!_userOp.callGasLimit || !_userOp.preVerificationGas || !_userOp.verificationGasLimit) {
+    const gasEst = await estimateUserOperationGas({
+      entryPointAddress,
+      provider,
+      userOp: _userOp,
+    });
+    if (gasEst?.callGasLimit) _userOp.callGasLimit = gasEst.callGasLimit;
+    if (gasEst?.verificationGasLimit) _userOp.verificationGasLimit = gasEst.verificationGasLimit;
+    if (gasEst?.preVerificationGas) _userOp.preVerificationGas = gasEst.preVerificationGas;
+  }
+  if (userOpVersion === '0.6.0') {
+    const _userOp060 = _userOp as UserOpv060;
+
+    if (!('initCode' in _userOp)) {
+      _userOp060.initCode = await getUserOpInitCode({
+        accountAddress: _userOp.sender,
+        provider,
+      });
+    }
+  }
+
+  // TODO Decode userOp to get token, pool and amount and validate there is nothing extra in the userOp
+  // Also the calldata will define the smart account implementation for initCode
+
+  // Simulate userOp
+  const simulation = await simulateUserOp({
+    provider,
+    entryPoint: entryPointAddress,
+    userOp: _userOp,
+  });
+  validateSimulation({
+    aaveATokens,
+    aavePoolAddress,
+    simulation,
+    entryPointAddress,
+    userOp: _userOp,
+  });
+
+  return {
+    simulationChanges: simulation.changes,
+    userOp: _userOp,
+  };
 };
--- packages/apps/ability-aave-smart-account/src/lib/vincent-ability.ts
+++ packages/apps/ability-aave-smart-account/src/lib/vincent-ability.ts
@@ -11,95 +11,9 @@ import {
   precheckSuccessSchema,
   abilityParamsSchema,
 } from './schemas';
-import { getAaveAddresses, getATokens } from './helpers/aave';
-import {
-  assertValidEntryPointAddress,
-  getSmartAccountNonce,
-  hashUnpackedUserOp,
-} from './helpers/entryPoint';
-import {
-  estimateUserOperationGas,
-  getUserOpInitCode,
-  getUserOpVersion,
-  simulateUserOp,
-  UserOp,
-  UserOpv060,
-} from './helpers/userOperation';
-import { validateSimulation } from './helpers/validation';
-
-interface ProccessUserOpParams {
-  entryPointAddress: string;
-  userOp: UserOp;
-  rpcUrl: string;
-}
-
-async function validateUserOp(params: ProccessUserOpParams) {
-  const { entryPointAddress, userOp, rpcUrl } = params;
-  const _userOp = { ...userOp };
-
-  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
-
-  await assertValidEntryPointAddress(entryPointAddress, provider);
-
-  const network = await provider.detectNetwork();
-  const { POOL: aavePoolAddress } = getAaveAddresses(network.chainId);
-  const aaveATokens = getATokens(network.chainId);
-
-  const userOpVersion = getUserOpVersion(_userOp);
-
-  // Complete userOp optional fields
-  if (!_userOp.nonce) {
-    _userOp.nonce = ethers.utils.hexValue(
-      await getSmartAccountNonce({
-        entryPointAddress,
-        provider,
-        accountAddress: _userOp.sender,
-      }),
-    );
-  }
-  if (!_userOp.callGasLimit || !_userOp.preVerificationGas || !_userOp.verificationGasLimit) {
-    const gasEst = await estimateUserOperationGas({
-      entryPointAddress,
-      provider,
-      userOp: _userOp,
-    });
-    if (gasEst?.callGasLimit) _userOp.callGasLimit = gasEst.callGasLimit;
-    if (gasEst?.verificationGasLimit) _userOp.verificationGasLimit = gasEst.verificationGasLimit;
-    if (gasEst?.preVerificationGas) _userOp.preVerificationGas = gasEst.preVerificationGas;
-  }
-  if (userOpVersion === '0.6.0') {
-    const _userOp060 = _userOp as UserOpv060;
-
-    if (!('initCode' in _userOp)) {
-      _userOp060.initCode = await getUserOpInitCode({
-        accountAddress: _userOp.sender,
-        provider,
-      });
-    }
-  }
-
-  // TODO Decode userOp to get token, pool and amount and validate there is nothing extra in the userOp
-  // Also the calldata will define the smart account implementation for initCode
-
-  // Simulate userOp
-  const simulation = await simulateUserOp({
-    provider,
-    entryPoint: entryPointAddress,
-    userOp: _userOp,
-  });
-  validateSimulation({
-    aaveATokens,
-    aavePoolAddress,
-    simulation,
-    entryPointAddress,
-    userOp: _userOp,
-  });
-
-  return {
-    simulationChanges: simulation.changes,
-    userOp: _userOp,
-  };
-}
+import { hashUnpackedUserOp } from './helpers/entryPoint';
+import { getUserOpVersion, UserOpv060 } from './helpers/userOperation';
+import { validateUserOp } from './helpers/validation';
 
 export const vincentAbility = createVincentAbility({
   packageName: '@lit-protocol/vincent-ability-aave-smart-account' as const,
