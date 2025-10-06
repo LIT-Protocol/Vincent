d9efcaa04c664eff3989d5d7e9b1c54652211f05
FedericoAmura <federicoamura@gmail.com>
2025-10-06 20:35:37 +0200

    fix: hex regex filtering values that had uppercase chars

--- packages/apps/ability-aave-smart-account/src/lib/helpers/userOperation.ts
+++ packages/apps/ability-aave-smart-account/src/lib/helpers/userOperation.ts
@@ -8,37 +8,37 @@ const userOpBaseSchema = z.object({
     .describe('The account making the operation'),
   nonce: z
     .string()
-    .regex(/^0x([1-9a-f]+[0-9a-f]*|0)$/)
+    .regex(/^0x([1-9a-fA-F]+[0-9a-fA-F]*|0)$/)
     .optional()
     .describe('Account nonce or creation salt'),
   callData: z
     .string()
-    .regex(/^0x[0-9a-f]*$/)
+    .regex(/^0x[0-9a-fA-F]*$/)
     .describe('Data for operation call'),
   callGasLimit: z
     .string()
-    .regex(/^0x([1-9a-f]+[0-9a-f]{0,15})|0$/)
+    .regex(/^0x([1-9a-fA-F]+[0-9a-fA-F]{0,15})|0$/)
     .optional()
     .describe('Gas allocated for call'),
   verificationGasLimit: z
     .string()
-    .regex(/^0x([1-9a-f]+[0-9a-f]{0,15})|0$/)
+    .regex(/^0x([1-9a-fA-F]+[0-9a-fA-F]{0,15})|0$/)
     .optional()
     .describe('Gas allocated for verification'),
   preVerificationGas: z
     .string()
-    .regex(/^0x([1-9a-f]+[0-9a-f]{0,15})|0$/)
+    .regex(/^0x([1-9a-fA-F]+[0-9a-fA-F]{0,15})|0$/)
     .optional()
     .describe('Gas for pre-verification execution and calldata'),
   maxFeePerGas: z
     .string()
-    .regex(/^0x([1-9a-f]+[0-9a-f]{0,15})|0$/)
+    .regex(/^0x([1-9a-fA-F]+[0-9a-fA-F]{0,15})|0$/)
     .optional()
     .default('0x59682F00') // 1.5 gwei (adjust to network conditions)
     .describe('Maximum fee per gas (EIP-1559)'),
   maxPriorityFeePerGas: z
     .string()
-    .regex(/^0x([1-9a-f]+[0-9a-f]{0,15})|0$/)
+    .regex(/^0x([1-9a-fA-F]+[0-9a-fA-F]{0,15})|0$/)
     .optional()
     .default('0x3B9ACA00') // 1 gwei
     .describe('Max priority fee per gas (EIP-1559)'),
@@ -51,7 +51,7 @@ const userOpBaseSchema = z.object({
     .object({
       chain_id: z
         .string()
-        .regex(/^0x([1-9a-f]+[0-9a-f]{0,15})|0$/)
+        .regex(/^0x([1-9a-fA-F]+[0-9a-fA-F]{0,15})|0$/)
         .optional()
         .describe('The chain Id of the authorization'),
       address: z
@@ -61,22 +61,22 @@ const userOpBaseSchema = z.object({
         .describe('The address of the authorization'),
       nonce: z
         .string()
-        .regex(/^0x([1-9a-f]+[0-9a-f]{0,15})|0$/)
+        .regex(/^0x([1-9a-fA-F]+[0-9a-fA-F]{0,15})|0$/)
         .optional()
         .describe('The nonce for the authorization'),
       y_parity: z
         .string()
-        .regex(/^0x([1-9a-f]+[0-9a-f]*|0)$/)
+        .regex(/^0x([1-9a-fA-F]+[0-9a-fA-F]*|0)$/)
         .optional()
         .describe('Y parity of signed authorization tuple'),
       r: z
         .string()
-        .regex(/^0x([1-9a-f]+[0-9a-f]{0,31})|0$/)
+        .regex(/^0x([1-9a-fA-F]+[0-9a-fA-F]{0,31})|0$/)
         .optional()
         .describe('R of signed authorization tuple'),
       s: z
         .string()
-        .regex(/^0x([1-9a-f]+[0-9a-f]{0,31})|0$/)
+        .regex(/^0x([1-9a-fA-F]+[0-9a-fA-F]{0,31})|0$/)
         .optional()
         .describe('S of signed authorization tuple'),
     })
@@ -86,14 +86,14 @@ const userOpBaseSchema = z.object({
 export const userOpv060Schema = userOpBaseSchema.extend({
   initCode: z
     .string()
-    .regex(/^0x[0-9a-f]*$/)
+    .regex(/^0x[0-9a-fA-F]*$/)
     .default('0x')
     .describe(
       'The initCode of the account if the account is not yet on-chain and needs creation. If the account is already on-chain, omit this field.',
     ),
   paymasterAndData: z
     .string()
-    .regex(/^0x[0-9a-f]*$/)
+    .regex(/^0x[0-9a-fA-F]*$/)
     .optional()
     .default('0x')
     .describe('Paymaster address and extra data'),
@@ -109,12 +109,12 @@ export const userOpv070Schema = userOpBaseSchema.extend({
     .describe('Paymaster contract address'),
   paymasterData: z
     .string()
-    .regex(/^0x[0-9a-f]*$/)
+    .regex(/^0x[0-9a-fA-F]*$/)
     .optional()
     .describe('Data for paymaster'),
   paymasterVerificationGasLimit: z
     .string()
-    .regex(/^0x([1-9a-f]+[0-9a-f]{0,15})|0$/)
+    .regex(/^0x([1-9a-fA-F]+[0-9a-fA-F]{0,15})|0$/)
     .optional()
     .describe('The gas limit for paymaster verification.'),
   factory: z
@@ -126,12 +126,12 @@ export const userOpv070Schema = userOpBaseSchema.extend({
     ),
   factoryData: z
     .string()
-    .regex(/^0x[0-9a-f]*$/)
+    .regex(/^0x[0-9a-fA-F]*$/)
     .optional()
     .describe('Data for the account factory (only if the account factory exists)'),
   paymasterPostOpGasLimit: z
     .string()
-    .regex(/^0x([1-9a-f]+[0-9a-f]{0,15})|0$/)
+    .regex(/^0x([1-9a-fA-F]+[0-9a-fA-F]{0,15})|0$/)
     .optional()
     .describe(
       'The amount of gas to allocate for the paymaster post-op code (only if a paymaster exists)',
