3696a351f0497772c2bb1ce38ff034cdd9fe2eb9
Wyatt Barnes <me@wyatt.email>
2025-11-18 17:53:02 -1000

    fix: eslint errors

--- packages/libs/contracts-sdk/eslint.config.js
+++ packages/libs/contracts-sdk/eslint.config.js
@@ -6,4 +6,14 @@ module.exports = [
   {
     ignores: ['**/lib/**/*'],
   },
+  {
+    files: ['package.json'],
+    rules: {
+      // Fix for this incorrect eslint error:
+      // packages/libs/contracts-sdk/package.json
+      //  32:3  error  The "contracts-sdk" project uses the following packages, but they are missing from "dependencies":
+      //   - @lit-protocol/types  @nx/dependency-checks
+      '@nx/dependency-checks': ['error', { ignoredDependencies: ['@lit-protocol/types'] }],
+    },
+  },
 ];
