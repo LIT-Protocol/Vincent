7a0f29ca116f587a8404711c13caca14a515d396
awisniew207 <andrew.wisniewski.08@gmail.com>
2025-12-01 19:37:29 +1000

    chore(relay-link-ability): fix test configuration issues

--- packages/apps/ability-relay-link-smart-account/jest.config.js
+++ packages/apps/ability-relay-link-smart-account/jest.config.js
@@ -1,6 +1,15 @@
+const path = require('path');
+
 module.exports = {
+  displayName: 'ability-relay-link-smart-account',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/.pnpm/(?!(@noble\\+secp256k1|cbor2|@cto\\.af\\+wtf8|@account-kit\\+[^@]+|@aa-sdk\\+[^@]+|@lit-protocol\\+vincent-scaffold-sdk|@t3-oss\\+[^@]+)@)',
+    `${path.join(__dirname, '../..')}/node_modules/.pnpm/(?!(@noble\\+secp256k1|cbor2|@cto\\.af\\+wtf8|@account-kit\\+[^@]+|@aa-sdk\\+[^@]+|@lit-protocol\\+vincent-scaffold-sdk|@t3-oss\\+[^@]+)@)`,
+    'node_modules/(?!.pnpm|@noble/secp256k1|cbor2|@cto\\.af/wtf8|@account-kit/[^/]+|@aa-sdk/[^/]+|@lit-protocol/vincent-scaffold-sdk|@t3-oss/[^/]+)',
+  ],
+  detectOpenHandles: true,
   passWithNoTests: true,
 };
