f8057c018495080d07c1e393dbd4af3ff960de43
awisniew207 <awisniew@uoregon.edu>
2026-01-07 13:00:30 -0800

    chore(registry-backend): remove unnecessary envs

--- packages/apps/registry-backend/src/env.ts
+++ packages/apps/registry-backend/src/env.ts
@@ -27,8 +27,6 @@ export const env = createEnv({
     MONGO_DB_NAME: z.string().optional(),
     PORT: z.coerce.number().default(3000),
     EXPECTED_AUDIENCE: z.string(),
-    LIT_RELAYER_API_KEY: z.string(),
-    LIT_PAYER_SECRET_KEY: z.string(),
     LIT_TXSENDER_RPC_URL: z.string().url(),
     LIT_TXSENDER_PRIVATE_KEY: z.string(),
     GAS_LIMIT_INCREASE_PERCENTAGE: z.coerce.number().default(110),
